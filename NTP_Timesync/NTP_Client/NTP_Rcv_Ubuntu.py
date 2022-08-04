514ab0d2301c755d20faff5fd94730a21229660a
deepanshu-UI <104488119+deepanshu-UI@users.noreply.github.com>
2022-08-04 17:45:53 +0530

    Add files via upload

--- NTP_Timesync/NTP_Client/NTP_Rcv_Ubuntu.py
+++ NTP_Timesync/NTP_Client/NTP_Rcv_Ubuntu.py
@@ -9,11 +9,14 @@ from socket import AF_INET, SOCK_DGRAM
 import os
 import socket
 import struct, time
+import subprocess
+import json
 # from datetime import datetime
 # import win32api
  
-def getNTPTime(host = "192.168.10.225"):
-        port = 123
+def getNTPTime(host, port):
+        host=host
+        port = port
         buf = 1024
         address = (host,port)
         msg = '\x1b' + 47 * '\0'
@@ -27,13 +30,37 @@ def getNTPTime(host = "192.168.10.225"):
         msg, address = client.recvfrom( buf )
         #print(msg,type(msg))
         t = struct.unpack( "!12I", msg )[10]
+        print("t: ", t)
         t -= TIME1970
-        #print(time.ctime(t),type(t))
-        return time.ctime(t).replace("  "," ")
+        t1 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
+        print("'"+t1+"'")
+        return "'"+t1+"'"
+        # print(time.ctime(t).replace("  "," "))
+        # return time.ctime(t).replace("  "," ")
         #return t
 
+
 if __name__ == "__main__":
-    a=getNTPTime()
-    print(a)
-    #os.system('sudo date -s {}'.format(a))
+
+    try:
+        with open('Ubuntu_Client.json', 'r') as f:
+            NTPConf = json.load(f)
+
+        host_ip = NTPConf["HOST_IP"]
+        port = NTPConf["Port"]
+
+        print('Parameters set successfully.')
+
+    except Exception as e:
+        print('Error in reading json file.')
+
+
+    a=getNTPTime(host_ip, port)
+    # print('a: ', a)
+#     os.system('sudo date --set={}'.format(a))
+    # b = "'2021-07-20 14:58:14'"
+    # cmd = "sudo date -s {}".format(a)
+    # subprocess.run(cmd, shell=True, check=True)
+    # os.system(cmd)
+    os.system("sudo date -s {}".format(a))
     
